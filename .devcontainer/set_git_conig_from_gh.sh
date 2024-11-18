74ec14b44ac25f2c260e32543dcec0e72cedbdf2
viridis00000 <viridis00000@gmail.com>
2024-11-19 02:40:49 +0900

    Allow unprotected redis (#5)
    * Adds API beta features to batch request execution
    
    Includes beta features setting in batch request execution
    to enhance functionality and support for new API features.
    
    No related issue.
    
    * Refactors settings file structure for clarity and accessibility
    
    Moves the settings file to a more accessible location to simplify configuration. Updates the retry delay and eta delay settings for better operational control of the worker queue.
    
    Improves readability and maintainability of the configuration process.
    
    * Adds Redis protected mode configuration
    
    Implements support for Redis protected mode by adding
    the relevant configuration in the post-start script.
    Ensures Redis binds to all interfaces for accessibility.
    
    Updates the setup settings to specify disabled
    protected mode in the YAML config.
    
    Enhances the supervisord configuration to include the
    protected mode parameter during Redis server startup.

--- .devcontainer/set_git_conig_from_gh.sh
+++ .devcontainer/set_git_conig_from_gh.sh
@@ -4,3 +4,4 @@ email=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version
 echo "Setting $user <$email> as the default Git user..."
 git config --global user.name "$user"
 git config --global user.email "$email"
+gh auth setup-git
